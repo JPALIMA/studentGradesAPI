b83e1f73020c905d0549c02fe7a5ded5c02e3c00
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-08-26 13:22:10 -0300

    configurando o controlador classe 'StudentsController'.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,33 +1,13 @@
+# app/controllers/students_controller.rb
 class StudentsController < ApplicationController
      def index
-          students = Student.all
-          render json: students
+       students = Student.all
+       render json: students
      end
-
+   
      def show
-          student = Student.find(params[:id])
-          render json: student
+       student = Student.find(params[:id])
+       render json: student
      end
-end
-
-class GradesController < AplicationController
-     def index
-          grades = Grade.all
-          render json: grades
-     end
-
-     def create
-          grade = Grade.new(grade_params)
-          if grade.save
-               render json: grade, status: :created
-          else
-               render json: grade.errors, status: :unprocessable_entity
-          end
-     end
-
-     private
-
-     def grade_params
-          params.require(:post).permit(:student_id, :subject, :value)
-     end
-end
+   end
+   
