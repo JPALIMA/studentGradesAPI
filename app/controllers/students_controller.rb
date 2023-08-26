dd1759e5fbe6898cf9a902c770d69ab3db225231
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-08-26 13:59:24 -0300

    configurando o controlador classe 'StudentsController'.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,13 +1,12 @@
 # app/controllers/students_controller.rb
 class StudentsController < ApplicationController
-     def index
-       students = Student.all
-       render json: students
-     end
-   
-     def show
-       student = Student.find(params[:id])
-       render json: student
-     end
-   end
-   
+  def index
+    students = Student.all
+    render json: students
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
+end
