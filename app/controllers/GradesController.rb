207ae67c431acb1d4fe064467cc8e0ab704c8f1a
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-08-26 13:40:14 -0300

    configurando o controlador classe 'GradeController'.

---
+++ app/controllers/GradesController.rb
@@ -0,0 +1,22 @@
+# app/controllers/grades_controller.rb
+class GradesController < ApplicationController
+  def index
+    grades = Grade.all
+    render json: grades
+  end
+
+  def create
+    grade = Grade.new(grade_params)
+    if grade.save
+      render json: grade, status: :created
+    else
+      render json: grade.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def grade_params
+    params.require(:grade).permit(:student_id, :subject, :value)
+  end
+end
